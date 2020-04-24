98f69035b61ec9be6fe54a83bb605536ae61f21d
Master-Hun <masterh8887@gmail.com>
2020-04-24 14:47:06 +0900

    test8

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -14,12 +14,5 @@ export class Hello extends React.Component < HelloProps, {}> {
   }
 }
 
-console.log("test");
-console.log("test");
-console.log("test11");
-console.log("test");
-console.log("test");
-console.log("test");
-console.log("test");
-console.log("test");
+
 
